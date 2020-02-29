175d5e5e5108b1cc15775304b4ea1e47bc658eab
yug <yuge@csplan.jp>
2020-02-29 21:11:22 +0900

    新規登録した場合のリダイレクトを/homeからユーザページに変更

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/talk';
 
     /**
      * Create a new controller instance.
