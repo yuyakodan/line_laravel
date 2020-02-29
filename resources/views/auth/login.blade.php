97e592ffc29556cf7362ee602bd892aedaf6f5af
小段悠也 <yuya.kodan@gmail.com>
2020-02-29 22:28:52 +0900

    ログイン・登録画面CSS追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,64 +1,92 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="wrapper">
-    <div class="title">
-        <h1>チャットしてく？？</h1>
-    </div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <div class="account">
-        <a href="{{ route('register')}} "class="float-right">新規アカウント制作</a>
-        <h1>ログイン</h1>
-        <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-            @csrf
-            <table class="table table-borderless">
-                <tr>
-                    <th>
-                        <p>メールアドレス</p>
-                    </th>
-                    <td>
-                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        <p>パスワード</p>
-                    </th>
-                    <td>
-                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-                        @if ($errors->has('password'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        <p>自動でログイン</p>
-                    </th>
-                    <td>
-                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-                        <label class="form-check-label" for="remember">
-                        </label>
-                    </td>
-                </tr>
-            </table>
+    <!-- Styles -->
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
 
-            <div class="col-md-8 offset-md-4">
-                <button type="submit" class="login btn btn-info">
-                    ログイン
-                </button>
+        <main class="py-4">
+
+            <div class="wrapper">
+                <div class="title">
+                    <h1>チャットしてく？？</h1>
+                </div>
+
+                <div class="account">
+                    <a href="{{ route('register')}} "class="float-right">新規アカウント制作</a>
+                    <h1>ログイン</h1>
+                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                        @csrf
+                        <table class="table table-borderless">
+                            <tr>
+                                <th>
+                                    <p>メールアドレス</p>
+                                </th>
+                                <td>
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <p>パスワード</p>
+                                </th>
+                                <td>
+                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <p>自動でログイン</p>
+                                </th>
+                                <td>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                    </label>
+                                </td>
+                            </tr>
+                        </table>
+
+                        <div class="col-md-8 offset-md-4">
+                            <button type="submit" class="login btn btn-info">
+                                ログイン
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
+        </main>
     </div>
-</div>
-@endsection
+</body>
+</html>
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,70 +1,97 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="wrapper">
-    <div class="title">
-        <h1>アカウント作ってく？？</h1>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+
+        <main class="py-4">
+            <div class="wrapper">
+                <div class="title">
+                    <h1>アカウント作ってく？？</h1>
+                </div>
+
+                <div class="account">
+                    <a href="{{ route('login')}}" class="float-right">ログイン</a>
+                    <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+                        @csrf
+                        <h1>新規アカウント作成</h1>
+                        <table class="table table-borderless">
+                            <tr>
+                                <th>
+                                    <p>ユーザー名</p>
+                                </th>
+                                <td>
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <p>メールアドレス</p>
+                                </th>
+                                <td>
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <p>パスワード</p>
+                                </th>
+                                <td>
+                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <p>パスワード（再入力）</p>
+                                </th>
+                                <td>
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                </td>
+                            </tr>
+                        </table>
+                        <button type="submit" class="login btn btn-info">作成</button>
+                    </form>
+                </div>
+            </div>
+        </main>
     </div>
+</body>
+</html>
 
-    <div class="account">
-        <a href="{{ route('login')}}" class="float-right">ログイン</a>
-        <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
-            @csrf
-            <h1>新規アカウント作成</h1>
-            <table class="table table-borderless">
-                <tr>
-                    <th>
-                        <p>ユーザー名</p>
-                    </th>
-                    <td>
-                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                    @if ($errors->has('name'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('name') }}</strong>
-                        </span>
-                    @endif
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        <p>メールアドレス</p>
-                    </th>
-                    <td>
-                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        <p>パスワード</p>
-                    </th>
-                    <td>
-                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                        @if ($errors->has('password'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                    </td>
-                </tr>
-                <tr>
-                    <th>
-                        <p>パスワード（再入力）</p>
-                    </th>
-                    <td>
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                    </td>
-                </tr>
-            </table>
-            <button type="submit" class="login btn btn-info">作成</button>
-        </form>
-    </div>
-</div>
-@endsection
